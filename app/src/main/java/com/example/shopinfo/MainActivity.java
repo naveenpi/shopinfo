3697b21ee1ac4220b0472d461112b8a4073b60bc
saiNaveen <snbnmu@gmail.com>
2020-03-01 14:33:04 -0600

    implemented method onMenuItemClick in MainActivity.java

--- app/src/main/java/com/example/shopinfo/MainActivity.java
+++ app/src/main/java/com/example/shopinfo/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.shopinfo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
     public static final int HOME_PAGE_REQUEST=11;
     ImageButton navigation;
@@ -26,5 +27,20 @@ public class MainActivity extends AppCompatActivity {
         popup.setOnMenuItemClickListener(this);
         popup.inflate(R.menu.navigation_menu);
         popup.show();
+    }
+
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                return true;
+            case R.id.item2:
+                return true;
+            case R.id.item3:
+                return true;
+            default:
+                return true;
+        }
     }
 }
