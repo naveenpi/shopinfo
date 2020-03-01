12192307d96ed6054104cd8f4e923cf68aa198e2
chaitanyapopuri <54385244+chaitanyapopuri@users.noreply.github.com>
2020-03-01 14:10:54 -0600

    added signup method to login.java

--- app/src/main/java/com/example/shopinfo/login.java
+++ app/src/main/java/com/example/shopinfo/login.java
@@ -9,23 +9,31 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class login extends AppCompatActivity {
-    public static final int EXTRA_KEY=0;
-    Button loginbtn,signbtn;
-    EditText username,password;
+    public static final int EXTRA_KEY = 0;
+    Button loginbtn, signbtn;
+    EditText username, password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        loginbtn=findViewById(R.id.loginbtn);
-        signbtn=findViewById(R.id.signbtn);
-        username=findViewById(R.id.username);
-        password=findViewById(R.id.password);
+        loginbtn = findViewById(R.id.loginbtn);
+        signbtn = findViewById(R.id.signbtn);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
     }
 
     public void login(View view) {
-        Intent toMainActivity= new Intent();
-        setResult(EXTRA_KEY,toMainActivity);
+        Intent toMainActivity = new Intent();
+        setResult(EXTRA_KEY, toMainActivity);
         finish();
+    }
+
+    public void signup(View view) {
+        Intent signup = new Intent(this, Signup.class);
+        startActivity(signup);
+
+
     }
 }
