1f1bc57997cf573b7e3ec7e4304b9cf233a312b7
praneeth323 <46974375+praneeth323@users.noreply.github.com>
2020-03-01 15:05:50 -0600

    Created Seller Registration activity

---
+++ app/src/main/java/com/example/shopinfo/SellerRegestration.java
@@ -0,0 +1,14 @@
+package com.example.shopinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SellerRegestration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller_regestration);
+    }
+}
