0ac8793178cab8bc6f0d32ce26cb5e84d00443e5
saiNaveen <snbnmu@gmail.com>
2020-03-01 14:27:40 -0600

    declared button, Image button, constant

--- app/src/main/java/com/example/shopinfo/MainActivity.java
+++ app/src/main/java/com/example/shopinfo/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.shopinfo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int HOME_PAGE_REQUEST=11;
+    ImageButton navigation;
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
