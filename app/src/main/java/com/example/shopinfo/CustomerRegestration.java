d2a470de54c1c9d6c6dd27df7739a99fdbfa5122
praneeth323 <46974375+praneeth323@users.noreply.github.com>
2020-03-01 15:01:11 -0600

    Declared button, edit text, image button references to CustomerRegestration.java

--- app/src/main/java/com/example/shopinfo/CustomerRegestration.java
+++ app/src/main/java/com/example/shopinfo/CustomerRegestration.java
@@ -3,8 +3,14 @@ package com.example.shopinfo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class CustomerRegestration extends AppCompatActivity {
+    EditText firstName, lastName, mailId, userName, password, confirmPassword;
+    ImageButton profilePic;
+    Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
