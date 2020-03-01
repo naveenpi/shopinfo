591645f84e19f864b49720be8beca03d017c4f8d
praneeth323 <46974375+praneeth323@users.noreply.github.com>
2020-03-01 15:02:22 -0600

    Initialized all the references that are declared

--- app/src/main/java/com/example/shopinfo/CustomerRegestration.java
+++ app/src/main/java/com/example/shopinfo/CustomerRegestration.java
@@ -16,5 +16,14 @@ public class CustomerRegestration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_regestration);
+        firstName=findViewById(R.id.editText);
+        lastName=findViewById(R.id.editText2);
+        mailId=findViewById(R.id.editText3);
+        userName=findViewById(R.id.editText4);
+        password=findViewById(R.id.editText5);
+        confirmPassword=findViewById(R.id.editText6);
+
+        profilePic=findViewById(R.id.profile);
+        register=findViewById(R.id.button);
     }
 }
