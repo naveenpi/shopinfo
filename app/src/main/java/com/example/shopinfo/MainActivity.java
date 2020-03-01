68fbb3d42236b31918952669a65a807216be3607
saiNaveen <snbnmu@gmail.com>
2020-03-01 14:28:52 -0600

    added menu method to MainActivity.java

--- app/src/main/java/com/example/shopinfo/MainActivity.java
+++ app/src/main/java/com/example/shopinfo/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.shopinfo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navigation=findViewById(R.id.navigation);
+        login=findViewById(R.id.login);
+    }
+
+    public void menu(View v) {
+        PopupMenu popup= new PopupMenu(this,v);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.navigation_menu);
+        popup.show();
     }
 }
