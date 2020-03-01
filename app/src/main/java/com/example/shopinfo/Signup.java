14aa1e019157da949647ea402565a565c1637f2a
pavanreddy4227 <54386960+pavanreddy4227@users.noreply.github.com>
2020-03-01 14:43:08 -0600

    Declared customer, seller buttons to Signup.java

--- app/src/main/java/com/example/shopinfo/Signup.java
+++ app/src/main/java/com/example/shopinfo/Signup.java
@@ -3,9 +3,10 @@ package com.example.shopinfo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Signup extends AppCompatActivity {
-
+    Button customer, seller;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
