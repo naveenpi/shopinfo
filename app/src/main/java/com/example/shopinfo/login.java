2f0849468017479c0f4c965764cd0622bf6bab42
chaitanyapopuri <54385244+chaitanyapopuri@users.noreply.github.com>
2020-03-01 14:08:44 -0600

    declared buttons, editTexts, constant

--- app/src/main/java/com/example/shopinfo/login.java
+++ app/src/main/java/com/example/shopinfo/login.java
@@ -3,9 +3,13 @@ package com.example.shopinfo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class login extends AppCompatActivity {
-
+    public static final int EXTRA_KEY=0;
+    Button loginbtn,signbtn;
+    EditText username,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
