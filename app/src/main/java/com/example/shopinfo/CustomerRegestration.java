09da1844f8bec3e63dafb977005f30c72a8780e7
pavanreddy4227 <54386960+pavanreddy4227@users.noreply.github.com>
2020-03-01 14:48:34 -0600

    Created CustomerRegestration activity

---
+++ app/src/main/java/com/example/shopinfo/CustomerRegestration.java
@@ -0,0 +1,14 @@
+package com.example.shopinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerRegestration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_regestration);
+    }
+}
