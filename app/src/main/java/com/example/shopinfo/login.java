693e8d41553fabf00fd41a88cabb769cd85809fe
chaitanyapopuri <54385244+chaitanyapopuri@users.noreply.github.com>
2020-03-01 14:09:51 -0600

    added login method to login.java

--- app/src/main/java/com/example/shopinfo/login.java
+++ app/src/main/java/com/example/shopinfo/login.java
@@ -2,7 +2,9 @@ package com.example.shopinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -14,5 +16,16 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginbtn=findViewById(R.id.loginbtn);
+        signbtn=findViewById(R.id.signbtn);
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+    }
+
+    public void login(View view) {
+        Intent toMainActivity= new Intent();
+        setResult(EXTRA_KEY,toMainActivity);
+        finish();
     }
 }
