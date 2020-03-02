51341f953df7a554d331401aceae6facda43ac7f
chaitanyapopuri <54385244+chaitanyapopuri@users.noreply.github.com>
2020-03-02 17:52:51 -0600

    added text box to the activity_about.xml

--- app/src/main/java/com/example/shopinfo/About.java
+++ app/src/main/java/com/example/shopinfo/About.java
@@ -3,6 +3,7 @@ package com.example.shopinfo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class About extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    public void menu(View view) {
     }
 }
