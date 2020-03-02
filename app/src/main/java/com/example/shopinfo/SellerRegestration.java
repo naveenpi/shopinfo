90c342f5f4988eec20b69ce444f4a4f8e127e0e2
saiNaveen <snbnmu@gmail.com>
2020-03-01 18:50:00 -0600

    Declared the variables and initialized for SellerRegestration.java

--- app/src/main/java/com/example/shopinfo/SellerRegestration.java
+++ app/src/main/java/com/example/shopinfo/SellerRegestration.java
@@ -3,12 +3,25 @@ package com.example.shopinfo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class SellerRegestration extends AppCompatActivity {
-
+    EditText shopName, category, address, phoneNumber, userName, password, confirmPassword;
+    ImageButton profilePicSeller;
+    Button registerSeller;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_regestration);
+
+        shopName=findViewById(R.id.shopName);
+        category=findViewById(R.id.category);
+        address=findViewById(R.id.address);
+        phoneNumber=findViewById(R.id.phone);
+        userName=findViewById(R.id.userNameSeller);;
+        password=findViewById(R.id.passwordSeller);
+        confirmPassword=findViewById(R.id.confirmPasswordSeller);
     }
 }
