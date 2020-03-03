2e77eade7817ce07d26c84a144eba232d08f9df5
pavanreddy4227 <54386960+pavanreddy4227@users.noreply.github.com>
2020-03-02 18:27:41 -0600

    updated switch case in Signup.java

--- app/src/main/java/com/example/shopinfo/Signup.java
+++ app/src/main/java/com/example/shopinfo/Signup.java
@@ -46,6 +46,8 @@ public class Signup extends AppCompatActivity implements PopupMenu.OnMenuItemCli
                 startActivity(toAbout);
                 return true;
             case R.id.item3:
+                Intent toShare=new Intent(this,Share.class);
+                startActivity(toShare);
                 return true;
             default:
                 return true;
