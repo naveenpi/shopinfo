c40c68c27e36fee2f6a8f52195ad0469cd75dd71
pavanreddy4227 <54386960+pavanreddy4227@users.noreply.github.com>
2020-03-02 18:20:00 -0600

    added navigation for share activity

---
+++ app/src/main/java/com/example/shopinfo/Share.java
@@ -0,0 +1,19 @@
+package com.example.shopinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Share extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+    }
+
+    public void menu(View view) {
+        
+    }
+}
